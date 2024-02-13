086a98d6f839ad4c55e31478dac887ad3924dce6
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-02-13 01:41:00 -0800

    initial commit

---
+++ utils/redis.js
@@ -0,0 +1,48 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (error) => {
+      console.error(`Error with Redis client: ${error}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    try {
+      const value = await getAsync(key);
+      return JSON.parse(value);
+    } catch (error) {
+      console.error(`Error getting value from Redis: ${error}`);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      const setAsync = promisify(this.client.set).bind(this.client);
+      await setAsync(key, JSON.stringify(value), 'EX', duration);
+    } catch (error) {
+      console.error(`Error setting value in Redis: ${error}`);
+    }
+  }
+
+  async del(key) {
+    try {
+      const delAsync = promisify(this.client.del).bind(this.client);
+      await delAsync(key);
+    } catch (error) {
+      console.error(`Error deleting value from Redis: ${error}`);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
